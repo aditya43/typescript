28002c66fb6efc716035dd520baeaa32cd4bd4e1
Aditya Hajare <aditya43@gmail.com>
2020-09-14 07:31:31 +0530

    Any type

---
+++ 01-Basics/06-Any.ts
@@ -0,0 +1,11 @@
+const Person: {
+    name: string;
+    age: any;
+    hobbies: any[];
+} = {
+    name: 'Aditya',
+    age: 'Hundred',
+    hobbies: ['Sports', 200, true]
+};
+
+console.log(Person)
