d275f77151278718eaa7ecf62111add3f994c7cf
Aditya Hajare <aditya43@gmail.com>
2020-09-19 18:10:07 +0530

    Inheritance

---
+++ 02-Classes-And-Interfaces/05-Inheritance.ts
@@ -0,0 +1,39 @@
+class User1 {
+    constructor(public name: string, private email: string) {}
+
+    print() {
+        console.log(`Name: ${this.name} | Email: ${this.email}`);
+    }
+}
+
+class Developer1 extends User1 {
+    public type: string = 'Developer';
+
+    constructor(name: string, email: string, private isActive: boolean) {
+        super(name, email);
+    }
+
+    print() {
+        super.print();
+        console.log(`type: ${this.type} | isActive: ${this.isActive}`);
+    }
+}
+
+class Tester1 extends User1 {
+    public type: string = 'Tester';
+
+    constructor(name: string, email: string, private isActive: boolean) {
+        super(name, email);
+    }
+
+    print() {
+        super.print();
+        console.log(`type: ${this.type} | isActive: ${this.isActive}`);
+    }
+}
+
+const u1 = new Developer1('Aditya', 'aditya@hajare.com', true);
+const u2 = new Tester1('Nishi', 'nishi@hajare.com', false);
+
+u1.print();
+u2.print();
