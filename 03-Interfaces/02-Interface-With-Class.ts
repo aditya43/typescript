a4bff281d8bde2ea724211776fcafc731422db15
Aditya Hajare <aditya43@gmail.com>
2020-09-20 13:36:12 +0530

    Interface with class

---
+++ 03-Interfaces/02-Interface-With-Class.ts
@@ -0,0 +1,26 @@
+interface Animal1 {
+    type: string;
+}
+
+interface Human1 {
+    name: string;
+    email: string;
+
+    greet(): void;
+}
+
+class User1 implements Animal1, Human1 {
+    constructor(
+        public type: string,
+        public name: string,
+        public email: string,
+    ) {}
+
+    greet(): void {
+        console.log(`${this.name} | ${this.email} | ${this.type}`);
+    }
+}
+
+const adi2 = new User1('Human', 'Aditya', 'aditya@hajare.com');
+
+adi2.greet();
