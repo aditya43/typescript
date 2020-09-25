ac3d731f4549cfb665320c26c420f40b8d3f52e9
Aditya Hajare <aditya43@gmail.com>
2020-09-25 15:50:31 +0530

    Generics functions

--- 05-Generics/02-Generics-Functions.ts
+++ 05-Generics/02-Generics-Functions.ts
@@ -1,5 +1,5 @@
-function merge<T, U>(a: T, b: U) {
-    return Object.assign(a, b);
+function merge<T extends object, U extends object>(a: T, b: U) {
+    return (<any>Object).assign(a, b);
 }
 
 const mergedObj = merge({ name: 'Aditya', city: 'Pune' }, { year: 2020 });
